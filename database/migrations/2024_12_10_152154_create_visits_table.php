df97b58556b7eb90c7774ca7d279abece958c56b
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-12-10 10:26:34 -0500

    feat: update fields of table visits migrate

--- database/migrations/2024_12_10_152154_create_visits_table.php
+++ database/migrations/2024_12_10_152154_create_visits_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('visits', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->float('latitude');
+            $table->float('longitude');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('visits');
