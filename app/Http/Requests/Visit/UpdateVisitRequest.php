04aab6ee8e6adcb6c36970571606aae61d1f41c7
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-12-10 12:28:01 -0500

    feat: add controller update

---
+++ app/Http/Requests/Visit/UpdateVisitRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests\Visit;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateVisitRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'nullable|string|max:255',
+            'email' => 'nullable|email|max:255',
+            'latitude' => 'nullable|numeric|between:-90,90',
+            'longitude' => 'nullable|numeric|between:-180,180',
+        ];
+    }
+}
