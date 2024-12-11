ab9f6ef53007d108f91c9ebaeca9c45926bc6d30
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-12-11 09:35:17 -0500

    feat: install vue 3, talwind and Leaflet

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 import './bootstrap';
+import '../css/app.css';
+
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import { InertiaProgress } from '@inertiajs/progress'
+
+InertiaProgress.init()
+
+createInertiaApp({
+    resolve: name => import(`./Pages/${name}.vue`),
+    setup({ el, App, props }) {
+        return createApp({ render: () => h(App, props) })
+            .mount(el)
+    },
+})
